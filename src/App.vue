c3d6e9f69f195ee110bac734f69d323891d1bae3
Roccofranchini <rocco.franchini1@gmail.com>
2021-09-17 14:04:50 +0200

    nav-added

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,7 @@
 <template>
 	<div id="app">
 		<Header />
+		<Nav />
 		<Main />
 		<Footer />
 	</div>
@@ -8,6 +9,7 @@
 
 <script>
 import Header from "./components/Header.vue";
+import Nav from "./components/Nav.vue";
 import Main from "./components/Main.vue";
 import Footer from "./components/Footer.vue";
 
@@ -15,6 +17,7 @@ export default {
 	name: "App",
 	components: {
 		Header,
+		Nav,
 		Main,
 		Footer,
 	},
---
+++ src/components/Nav.vue
@@ -0,0 +1,50 @@
+<template>
+	<nav id="nav">
+		<div
+			class="icons d-flex flex-column align-items-center shadow p-2 mb-5 bg-body rounded"
+		>
+			<a href=""><i class="fas fa-caret-up ruler"></i></a>
+			<a href=""><i class="far fa-life-ring lifering"></i></a>
+			<a href=""><i class="fas fa-book"></i></a>
+			<a href=""><i class="fas fa-shopping-cart"></i></a>
+		</div>
+	</nav>
+</template>
+
+<script>
+export default {
+	name: "Nav",
+};
+</script>
+
+<style scoped lang="scss">
+#nav {
+	position: fixed;
+	top: 150px;
+	right: 0;
+
+	.ruler {
+		font-size: 17px;
+		margin-bottom: 0;
+	}
+
+	.life-ring {
+		margin-top: 0;
+	}
+
+	a {
+		text-decoration: none;
+		color: black;
+		font-size: 13px;
+		margin: 3px;
+
+		i {
+			transition: transform;
+		}
+
+		i:hover {
+			transform: scale(1.5);
+		}
+	}
+}
+</style>
